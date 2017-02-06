9fb1ea59520613674e293b6409da22b1ca394241
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:46 +0800

    novnc: Generate vnc tokens dynamically
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ system/novnc/etc/startup.aux/generate-vnc-tokens.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# generate-vnc-tokens.sh -- Generate the "token: vnc-ip:vnc-port" map file
+#
+#   TODO: Use vnc-ip:vnc-port directly?
+#
+# By default, generate the token via md5sum the 'ip:port' string.
+#
+
+[ -z "$DEFAULT_GW" ] && DEFAULT_GW=$(route -n | grep "^0.0.0.0" | tr -s ' ' | cut -d' ' -f2)
+[ -z "$ENCRYPT_CMD" ] && ENCRYPT_CMD=md5sum
+
+port=5900
+map=/vnc-tokens.txt
+net=$(echo $DEFAULT_GW | cut -d'.' -f1-3)
+
+for i in `seq 1 255`; do
+    ip_port=$net.$i:$port
+    #token=$i; echo "$token: $ip_port"
+    token=$(echo -n $ip_port | $ENCRYPT_CMD | cut -d' ' -f1); echo "$token: $ip_port"
+done | tee $map
