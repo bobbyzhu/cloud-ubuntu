306ae9636132b3d54870bc77bc95c8fe12cf183f
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:46 +0800

    jekyll: try br0 if eth0 not exist
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/jekyll/etc/startup.aux/start-jekyll.sh
+++ system/jekyll/etc/startup.aux/start-jekyll.sh
@@ -6,7 +6,9 @@
 [ -z "$JEKYLL_SRC" ] && JEKYLL_SRC=~/jekyll-bootstrap/
 # For regular users, must bigger than 1024
 [ -z "$JEKYLL_PORT" ] && JEKYLL_PORT=8080
-[ -z "$JEKYLL_HOST" ] && JEKYLL_HOST=`ifconfig eth0 | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
+[ -z "$JEKYLL_IFACE" ] && JEKYLL_IFACE=eth0
+[ -z "$JEKYLL_HOST" ] && JEKYLL_HOST=`ifconfig $JEKYLL_IFACE | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
+[ -z "$JEKYLL_HOST" ] && JEKYLL_HOST=`ifconfig br0 | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
 
 [ -z "$UNIX_USER" ] && UNIX_USER=ubuntu
 
