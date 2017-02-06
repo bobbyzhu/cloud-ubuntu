d5b0af815b6bcf2f8e5e62d8ce1284041bdaa42d
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    Clean up the name of the proxy server start script
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/proxy_server/etc/startup.aux/start-proxy-server.sh
+++ system/proxy_server/etc/startup.aux/start-proxy-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# pwgen.sh -- set a password for proxy server
+# start-proxy-server.sh
 #
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../..
@@ -8,5 +8,7 @@ TOP_DIR=$(cd $(dirname $0) && pwd)/../..
 [ -z "$PROXY_PWD" ] && PROXY_PWD=`pwgen -c -n -s -1 15`
 [ -z "$PROXY_DNS" ] && PROXY_DNS=8.8.8.8
 [ -z "$PROXY_PORT" ] && PROXY_PORT=80
+
+echo "PROXY_PWD: $PROXY_PWD"
 
 HPROXY_PASS=$PROXY_PWD ${TOP_DIR}/usr/local/bin/hev-socks5-proxy 0.0.0.0 $PROXY_PORT $PROXY_DNS
