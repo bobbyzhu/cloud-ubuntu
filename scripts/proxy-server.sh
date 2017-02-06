ee567ed0c29770dd81e53224712e0bfce2a533d3
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    proxy server: Use cpu 0 by default
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/proxy-server.sh
+++ scripts/proxy-server.sh
@@ -9,4 +9,4 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
-PORT_MAP="-p 80:80" HOST_NAME=localhost EXTRA_ARGS="--cpuset-cpus=1 --memory=96M" ${TOP_DIR}/run proxy_server
+PORT_MAP="-p 80:80" HOST_NAME=localhost EXTRA_ARGS="--cpuset-cpus=0 --memory=96M" ${TOP_DIR}/run proxy_server
