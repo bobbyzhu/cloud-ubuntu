0ba0de4832f55a1b74d5267166352720292789c5
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:46 +0800

    jekyll: Let jekyll run with regular users
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/jekyll/etc/startup.aux/start-jekyll.sh
+++ system/jekyll/etc/startup.aux/start-jekyll.sh
@@ -4,7 +4,11 @@
 #
 
 [ -z "$JEKYLL_SRC" ] && JEKYLL_SRC=~/jekyll-bootstrap/
-[ -z "$JEKYLL_PORT" ] && JEKYLL_PORT=80
+# For regular users, must bigger than 1024
+[ -z "$JEKYLL_PORT" ] && JEKYLL_PORT=8080
 [ -z "$JEKYLL_HOST" ] && JEKYLL_HOST=`ifconfig eth0 | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
 
-/usr/local/bin/jekyll s -w --future --limit_posts 10 -s $JEKYLL_SRC -P $JEKYLL_PORT -H $JEKYLL_HOST
+[ -z "$UNIX_USER" ] && UNIX_USER=ubuntu
+
+cd $JEKYLL_SRC
+sudo -u $UNIX_USER /usr/local/bin/jekyll s -w --future --limit_posts 10 -s $JEKYLL_SRC -P $JEKYLL_PORT -H $JEKYLL_HOST
