3f0393bfc1f7ae364f63495565c8d6eb7e6e1321
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:46 +0800

    web: Slient the config output
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/web-ubuntu.sh
+++ scripts/web-ubuntu.sh
@@ -2,7 +2,7 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
-source ${TOP_DIR}/config $*
+source ${TOP_DIR}/config $* 2>&1 >/dev/null
 
 DEFAULT_PORT_MAP=" -p $LOCAL_VNC_PORT:$VNC_PORT -p $LOCAL_SSH_PORT:$SSH_PORT -p $LOCAL_WEBSSH_PORT:$WEBSSH_PORT "
 
