b052c6e9becc17d494b6e05af78ac498e2af4b1e
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:48 +0800

    web: vnc: Add session record and playback support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/web-ubuntu.sh
+++ scripts/web-ubuntu.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# web-ubuntu.sh
+#
+# 1. Record the screen session into noVNC/recordings/
+#
+#    $ VNC_RECORD=1 ./scripts/web-ubuntu.sh
+#
+# 2. Playback
+#
+#    http://localhost:6080/tests/vnc_playback.html?data=vnc.record.data.1&mode=realtime
+#
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
@@ -10,6 +21,19 @@ DEFAULT_PORT_MAP=" -p $LOCAL_VNC_PORT:$VNC_PORT -p $LOCAL_SSH_PORT:$SSH_PORT -p 
 [ -z "$ENCRYPT_CMD" ] && ENCRYPT_CMD="cat"
 [ -n "$UNIX_PWD" ] && UNIX_PWD=`echo -n $UNIX_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
 [ -n "$VNC_PWD" ] && VNC_PWD=`echo -n $VNC_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
+[ -z "$VNC_RECORD" ] && VNC_RECORD=0
+[ -z "$VNC_TIMEOUT" ] && VNC_TIMEOUT=0
+
+if [ $VNC_RECORD -eq 1 ]; then
+    VNC_RECORD_DIR=/noVNC/recordings
+    VNC_RECORD_FILE=$VNC_RECORD_DIR/vnc.record.data
+    LOCAL_RECORD_DIR=$TOP_DIR/$VNC_RECORD_DIR
+    [ ! -d $LOCAL_RECORD_DIR ] && mkdir $LOCAL_RECORD_DIR
+    VOLUME_MAP=" -v $LOCAL_RECORD_DIR:$VNC_RECORD_DIR "
+    echo "LOG: VNC screen recorded in $VNC_RECORD_FILE"
+fi
 
 echo "PORTS: VNC=$LOCAL_VNC_PORT SSH=$LOCAL_SSH_PORT WEBSSH=$LOCAL_WEBSSH_PORT"
-EXTRA_ARGS="-e PROXY_SPEED_LIMIT=0 -e NOSSL=$NOSSL $EXTRA_ARGS" UNIX_PWD="$UNIX_PWD" VNC_PWD="$VNC_PWD" PORT_MAP="$DEFAULT_PORT_MAP" ${TOP_DIR}/run web
+EXTRA_ARGS="$EXTRA_ARGS -e PROXY_SPEED_LIMIT=0 -e NOSSL=$NOSSL"
+EXTRA_ARGS="$EXTRA_ARGS -e VNC_RECORD=$VNC_RECORD -e VNC_TIMEOUT=$VNC_TIMEOUT -e VNC_RECORD_FILE=$VNC_RECORD_FILE $VOLUME_MAP" \
+    UNIX_PWD="$UNIX_PWD" VNC_PWD="$VNC_PWD" PORT_MAP="$DEFAULT_PORT_MAP" ${TOP_DIR}/run web
--- system/web/etc/startup.aux/novnc.sh
+++ system/web/etc/startup.aux/novnc.sh
@@ -6,9 +6,17 @@
 VNC_LAUNCH=/noVNC/utils/launch.sh
 
 [ -z "$VNC_TIMEOUT" ] && VNC_TIMEOUT=0
+[ -z "$VNC_RECORD" ] && VNC_RECORD=0
 
-if [ $VNC_TIMEOUT -eq 0 ]; then
-    sed -i -e "s%--timeout=1800%%g" $VNC_LAUNCH
-else
-    sed -i -e "s%--timeout=1800%--timeout=$VNC_TIMEOUT%g" $VNC_LAUNCH
+VNC_CONFIG=""
+
+if [ $VNC_TIMEOUT -eq 1 ]; then
+    VNC_CONFIG="$VNC_CONFIG --timeout=$VNC_TIMEOUT"
 fi
+
+if [ $VNC_RECORD -eq 1 ]; then
+    [ -z "$VNC_RECORD_FILE" ] && VNC_RECORD_FILE=/tmp/vnc.record.data
+    VNC_CONFIG="$VNC_CONFIG --record $VNC_RECORD_FILE"
+fi
+
+sed -i -e "s%--timeout=1800%$VNC_CONFIG%g" $VNC_LAUNCH
