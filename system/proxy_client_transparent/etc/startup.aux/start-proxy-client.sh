6656c1ed511b949964d2b5e129c58f11eef78dd4
Heiher <r@hev.cc>
2017-02-07 02:07:45 +0800

    Fix transparent proxy client support

--- system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
+++ system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
@@ -14,21 +14,16 @@ PROXY_SERVER="$(echo $PROXY_SERVER | tr ':' ' ')"
 
 [ -z "$PROXY_PORT" ] && PROXY_PORT=1080
 
-# 代理服务客户端，负责将接收到的 socks5 请求封装成 http 协议，转发给服务器端。
-${TOP_DIR}/usr/local/bin/hev-socks5-client 0.0.0.0 $PROXY_PORT $PROXY_PWD $PROXY_SERVER &
+HPROXY_PASS=$PROXY_PWD ${TOP_DIR}/usr/local/bin/hev-socks5-client 127.0.0.1 $PROXY_PORT $PROXY_SERVER &
 
-sleep 1
+${TOP_DIR}/usr/local/bin/hev-socks5-tproxy 0.0.0.0 10800 0.0.0.0 5300 127.0.0.1 $PROXY_PORT &
 
-# DNS 转发器，iptables 重定向 udp 53 端口的报文至此服务，dns-forwarder 转成 tcp 查询请求并重发出来。
-${TOP_DIR}/usr/local/bin/hev-dns-forwarder 0.0.0.0 5300 8.8.8.8 &
-sleep 1
+sudo iptables-restore ${TOP_DIR}/etc/iptables/tproxy.rules
+PROXY_SERVER_IP="$(echo $PROXY_SERVER | awk '{print $1}')"
+PROXY_SERVER_PORT="$(echo $PROXY_SERVER | awk '{print $2}')"
+sudo iptables -t nat -I OUTPUT -d $PROXY_SERVER_IP/32 -p tcp --dport $PROXY_SERVER_PORT -j RETURN
 
-# 透明代理服务，iptables 的 REDIRECT target 会将命中的 TCP 连接重定向至此服务，socks5-tproxy封装至 socks5 请求转发至 socks5-client。
-${TOP_DIR}/usr/local/bin/hev-socks5-tproxy 0.0.0.0 10800 0.0.0.0 $PROXY_PORT &
-sleep 1
-
-# 导入 iptables 规则。
-sudo iptables-restore ${TOP_DIR}/usr/local/bin/iptables-transparent-bypass.rules
+sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 
 gpasswd -d ubuntu adm
 gpasswd -d ubuntu sudo
