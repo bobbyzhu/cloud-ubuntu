d4b2230e2620a79e3cd2d54a32b1723e16235c7f
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:44 +0800

    Clean up the desktop configuration support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- startup.sh
+++ startup.sh
@@ -11,10 +11,7 @@ id -u ubuntu &>/dev/null || useradd --create-home --shell /bin/bash --user-group
 [ ! -d /home/ubuntu/.vnc ] && sudo -u ubuntu mkdir -p /home/ubuntu/.vnc
 sudo -u ubuntu /usr/bin/x11vnc -storepasswd $VNC_PWD  /home/ubuntu/.vnc/passwd
 
-sudo -u ubuntu -i bash -c "mkdir -p /home/ubuntu/.config/pcmanfm/LXDE/ \
-    && mkdir -p  /home/ubuntu/.config/lxpanel/LXDE/panels \
-    && cp /usr/share/desktop/desktop-items-0.conf /home/ubuntu/.config/pcmanfm/LXDE/ \
-    && cp /usr/share/desktop/panel /home/ubuntu/.config/lxpanel/LXDE/panels/panel"
+sudo -u ubuntu -i bash -c "cp -r /usr/share/desktop/.config /home/ubuntu/"
 
 # /tmp
 mount -t tmpfs none /tmp
