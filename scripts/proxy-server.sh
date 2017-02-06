b7dded3785870392b88221e485dfe058d8614898
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    script: Fix up for proxy server
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/proxy-server.sh
+++ scripts/proxy-server.sh
@@ -5,4 +5,4 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
-PORT_MAP="-p 80:80" HOST_NAME=localhost EXTRA_ARGS="--cpuset-cpus=1 --memory=96M" ${TOP_DIR}/run proxy
+PORT_MAP="-p 80:80" HOST_NAME=localhost EXTRA_ARGS="--cpuset-cpus=1 --memory=96M" ${TOP_DIR}/run proxy_server
