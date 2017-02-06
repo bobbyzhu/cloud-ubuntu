8ccae63ae54c2e8c9a5fe4527394c94b6ad20984
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    jekyll: Allow to pass arguments to scripts/jekyll.sh
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/jekyll.sh
+++ scripts/jekyll.sh
@@ -5,14 +5,14 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
-JEKYLL_GIT=https://github.com/plusjade/jekyll-bootstrap/
-JEKYLL_LOCAL=${TOP_DIR}/jekyll-bootstrap/
-JEKYLL_SRC=/home/ubuntu/jekyll-bootstrap/
-JEKYLL_PORT=8080
-JEKYLL_HOST=`ifconfig eth0 | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
+[ -z "$JEKYLL_GIT" ] && JEKYLL_GIT=https://github.com/plusjade/jekyll-bootstrap/
+[ -z "$JEKYLL_LOCAL" ] && JEKYLL_LOCAL=${TOP_DIR}/jekyll-bootstrap/
+[ -z "$JEKYLL_SRC" ] && JEKYLL_SRC=/home/ubuntu/jekyll-bootstrap/
+[ -z "$JEKYLL_PORT" ] && JEKYLL_PORT=8080
+[ -z "$JEKYLL_HOST" ] && JEKYLL_HOST=`ifconfig eth0 | grep "inet addr" | sed -e "s/ *inet addr:\([0-9\.]*\) .*/\1/g"`
 
 [ ! -d $JEKYLL_LOCAL/.git ] && git clone $JEKYLL_GIT $JEKYLL_LOCAL
 
-EXTRA_ARGS="-p $JEKYLL_PORT:80 -v $JEKYLL_LOCAL:$JEKYLL_SRC" ./run jekyll
+EXTRA_ARGS="-p $JEKYLL_PORT:80 -v $JEKYLL_LOCAL:$JEKYLL_SRC -e JEKYLL_SRC=$JEKYLL_SRC" ./run jekyll
 
 echo "LOG: URL: http://$JEKYLL_HOST:$JEKYLL_PORT"
