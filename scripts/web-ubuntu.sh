890498ba4858a529f8b9adb3ef257217b5592339
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:47 +0800

    web: Add unix/vnc password
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/web-ubuntu.sh
+++ scripts/web-ubuntu.sh
@@ -6,5 +6,10 @@ source ${TOP_DIR}/config $* 2>&1 >/dev/null
 
 DEFAULT_PORT_MAP=" -p $LOCAL_VNC_PORT:$VNC_PORT -p $LOCAL_WEBSSH_PORT:$WEBSSH_PORT "
 
+# Available encrypt cmds: sha1sum, sha224sum, cksum, sha256sum, sha512sum, md5sum, sha384sum, sum
+[ -z "$ENCRYPT_CMD" ] && ENCRYPT_CMD="cat"
+[ -n "$UNIX_PWD" ] && PROXY_PWD=`echo -n $UNIX_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
+[ -n "$VNC_PWD" ] && PROXY_PWD=`echo -n $VNC_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
+
 echo "PORTS: VNC=$LOCAL_VNC_PORT WEBSSH=$LOCAL_WEBSSH_PORT"
-PORT_MAP="$DEFAULT_PORT_MAP" ./run web
+UNIX_PWD="$UNIX_PWD" VNC_PWD="$VNC_PWD" PORT_MAP="$DEFAULT_PORT_MAP" ./run web
