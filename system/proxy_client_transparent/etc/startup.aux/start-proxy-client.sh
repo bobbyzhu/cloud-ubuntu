0ff4a4d56eef5606f63d8a6f6228f20ec0e8f8a7
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    tproxy: clean up proxy server support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
+++ system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
@@ -7,23 +7,23 @@ TOP_DIR=$(cd $(dirname $0) && pwd)/../../
 
 [ -z "$PROXY_SERVER" ] && \
     echo "Please run docker with ENV_ARGS='-e PROXY_SERVER=IP:PORT'" && exit 1
-PROXY_SERVER="$(echo $PROXY_SERVER | tr ':' ' ')"
 
 [ -z "$PROXY_PWD" ] && \
     echo "Please run docker with ENV_ARGS='-e PROXY_PWD=PASSWORD'" && exit 2
 
 [ -z "$PROXY_PORT" ] && PROXY_PORT=1080
 
-HPROXY_PASS=$PROXY_PWD ${TOP_DIR}/usr/local/bin/hev-socks5-client 127.0.0.1 $PROXY_PORT $PROXY_SERVER &
+PROXY_SERVER_IP=${PROXY_SERVER%:*}
+PROXY_SERVER_PORT=${PROXY_SERVER#*:}
+
+HPROXY_PASS=$PROXY_PWD ${TOP_DIR}/usr/local/bin/hev-socks5-client 127.0.0.1 $PROXY_PORT $PROXY_SERVER_IP $PROXY_SERVER_PORT &
 
 ${TOP_DIR}/usr/local/bin/hev-socks5-tproxy 0.0.0.0 10800 0.0.0.0 5300 127.0.0.1 $PROXY_PORT &
 
 sudo iptables-restore ${TOP_DIR}/etc/iptables/tproxy.rules
-PROXY_SERVER_IP="$(echo $PROXY_SERVER | awk '{print $1}')"
-PROXY_SERVER_PORT="$(echo $PROXY_SERVER | awk '{print $2}')"
 sudo iptables -t nat -I OUTPUT -d $PROXY_SERVER_IP/32 -p tcp --dport $PROXY_SERVER_PORT -j RETURN
 
 sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 
-gpasswd -d ubuntu adm
-gpasswd -d ubuntu sudo
+sudo gpasswd -d ubuntu adm
+sudo gpasswd -d ubuntu sudo
