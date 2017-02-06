82b00eb4cb1e965b9445d1aa26c6dab0a8ba64ee
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:47 +0800

    scripts: Fix up password
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/web-ubuntu.sh
+++ scripts/web-ubuntu.sh
@@ -8,8 +8,8 @@ DEFAULT_PORT_MAP=" -p $LOCAL_VNC_PORT:$VNC_PORT -p $LOCAL_SSH_PORT:$SSH_PORT -p 
 
 # Available encrypt cmds: sha1sum, sha224sum, cksum, sha256sum, sha512sum, md5sum, sha384sum, sum
 [ -z "$ENCRYPT_CMD" ] && ENCRYPT_CMD="cat"
-[ -n "$UNIX_PWD" ] && PROXY_PWD=`echo -n $UNIX_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
-[ -n "$VNC_PWD" ] && PROXY_PWD=`echo -n $VNC_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
+[ -n "$UNIX_PWD" ] && UNIX_PWD=`echo -n $UNIX_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
+[ -n "$VNC_PWD" ] && VNC_PWD=`echo -n $VNC_PWD | $ENCRYPT_CMD | cut -d' ' -f1`
 
 echo "PORTS: VNC=$LOCAL_VNC_PORT SSH=$LOCAL_SSH_PORT WEBSSH=$LOCAL_WEBSSH_PORT"
 EXTRA_ARGS="-e PROXY_SPEED_LIMIT=0" UNIX_PWD="$UNIX_PWD" VNC_PWD="$VNC_PWD" PORT_MAP="$DEFAULT_PORT_MAP" ${TOP_DIR}/run web
