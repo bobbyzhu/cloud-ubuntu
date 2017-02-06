4f7213e22a9455cc990a400724ee1e84665c46a9
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    novnc: Add https support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- gen-pem.sh
+++ gen-pem.sh
@@ -2,8 +2,12 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)
 
+KEY=${TOP_DIR}/system/base/etc/nginx/cert.key
+CERT=${TOP_DIR}/system/base/etc/nginx/cert.crt
 PEM=${TOP_DIR}/noVNC/utils/self.pem
 
 [ -n "$AUTO" -a "$AUTO" == "1"  ] && BATCH=-batch
 
-openssl req $BATCH -new -x509 -days 365 -nodes -out $PEM -keyout $PEM
+openssl req $BATCH -x509 -nodes -days 365 -newkey rsa:2048 -out $CERT -keyout $KEY
+
+cat $CERT $KEY > $PEM
