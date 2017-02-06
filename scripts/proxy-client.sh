8d94fce72627713f7725c12d6d7f9a38e8629f1e
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:07:45 +0800

    scripts: Document how to use proxy client
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/proxy-client.sh
+++ scripts/proxy-client.sh
@@ -6,6 +6,8 @@
 #
 #    EXTRA_ARGS="-p 1080:1080 -e PROXY_SERVER=IP:PORT -e PROXY_PWD=PASSWORD [-e PROXY_PORT=1080]" ./scripts/proxy-client.sh
 #
+# Note: To use this proxy in web browser, please set socks v5 proxy: localhost:1080, and enable "Remote DNS".
+#
 
 TOP_DIR=$(cd $(dirname $0) && pwd)/../
 
